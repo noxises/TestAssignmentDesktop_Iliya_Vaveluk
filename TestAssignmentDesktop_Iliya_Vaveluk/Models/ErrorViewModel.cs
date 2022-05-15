9417967b3685cab9675c8794dd3440f13ac870ad
simptom000 <simptom000@gmail.com>
2022-05-15 14:46:48 +0300

    Добавьте файлы проекта.

---
+++ TestAssignmentDesktop_Iliya_Vaveluk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestAssignmentDesktop_Iliya_Vaveluk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
